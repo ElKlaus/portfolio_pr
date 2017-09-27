b6f0bdeed45dd5028f97fc6bcc91b2182475526f
Sergey Kirov <klaus15rus@gmail.com>
2017-09-27 21:09:53 +0400

    WELCOM: begin

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,11 +1,11 @@
 import 'normalize.css';
 import './index.scss';
 
-import menuMacker from '../../components/menu/menu';
-let menu = menuMacker(['Главная','Обо мне','Блог'], 'menu');
-document.body.appendChild(menu);
+// import menuMacker from '../../components/menu/menu';
+// let menu = menuMacker(['Главная','Обо мне','Блог'], 'menu');
+// document.body.appendChild(menu);
 
-console.log('in index.js');
-console.log($);
+// console.log('in index.js');
+// console.log($);
 
 
